e6cd8c0b40cf1513a24a3a37ce8faa7ba1e93d9b
StalkR <stalkr@stalkr.net>
2013-11-11 21:16:46 +0100

    appengine: move app in subdir, add library and example

---
+++ appengine/api.go
@@ -0,0 +1,53 @@
+// Package appengine implements communication to imdb AppEngine app using JSON API.
+package appengine
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/StalkR/imdb"
+)
+
+// AppURL points to a running github.com/StalkR/imdb/appengine/app.
+const AppURL = "https://movie-db-api.appspot.com"
+
+// NewTitle obtains a Title ID with its information and returns a Title.
+func NewTitle(c *http.Client, id string) (*imdb.Title, error) {
+	resp, err := c.Get(fmt.Sprintf("%s/title/%s", AppURL, id))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var title imdb.Title
+	if err = json.Unmarshal(b, &title); err != nil {
+		return nil, err
+	}
+	return &title, nil
+}
+
+// SearchTitle searches a Title and returns a list of titles that matched.
+func SearchTitle(c *http.Client, q string) ([]imdb.Title, error) {
+	params := url.Values{}
+	params.Set("q", q)
+	resp, err := c.Get(fmt.Sprintf("%s/find?%s", AppURL, params.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var titles []imdb.Title
+	if err = json.Unmarshal(b, &titles); err != nil {
+		return nil, err
+	}
+	return titles, nil
+}
