1350f14a7ee2f6015017e633606f216997b30f30
StalkR <stalkr@stalkr.net>
2022-11-21 11:22:15 +0100

    test: set a chrome useragent or awswaf blocks sometimes

--- init_test.go
+++ init_test.go
@@ -8,32 +8,34 @@ import (
 	"github.com/StalkR/httpcache"
 )
 
+const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+
+const cacheTTL = 24 * time.Hour
+
 // client is used by tests to perform cached requests.
 // If cache directory exists it is used as a persistent cache.
 // Otherwise a volatile memory cache is used.
 var client *http.Client
 
-var ttl = 24 * time.Hour
-
 func init() {
 	if _, err := os.Stat("cache"); err == nil {
-		client, err = httpcache.NewPersistentClient("cache", ttl)
+		client, err = httpcache.NewPersistentClient("cache", cacheTTL)
 		if err != nil {
 			panic(err)
 		}
 	} else {
-		client = httpcache.NewVolatileClient(ttl, 1024)
+		client = httpcache.NewVolatileClient(cacheTTL, 1024)
 	}
-	client.Transport = &englishTransport{client.Transport}
+	client.Transport = &customTransport{client.Transport}
 }
 
-// englishTransport implements http.RoundTripper interface
-// to add "Accept-Language: en" header on all requests.
-type englishTransport struct {
+// customTransport implements http.RoundTripper interface to add some headers.
+type customTransport struct {
 	http.RoundTripper
 }
 
-func (e *englishTransport) RoundTrip(r *http.Request) (*http.Response, error) {
-	r.Header.Add("Accept-Language", "en")
+func (e *customTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Set("Accept-Language", "en") // avoid IP-based language detection
+	r.Header.Set("User-Agent", userAgent)
 	return e.RoundTripper.RoundTrip(r)
 }
