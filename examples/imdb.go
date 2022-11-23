2dbe03459363d8f1ba89130c2ebb4e31c23b3915
StalkR <stalkr@stalkr.net>
2022-11-23 08:59:36 +0100

    examples: show setting user-agent to avoid awswaf
    Fixes #10

--- examples/imdb.go
+++ examples/imdb.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/StalkR/imdb"
 )
@@ -17,7 +18,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	client := http.DefaultClient
+	client := &http.Client{
+		Transport: &customTransport{http.DefaultTransport},
+	}
 	title, err := imdb.SearchTitle(client, flag.Arg(0))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
@@ -34,4 +37,19 @@ func main() {
 	}
 
 	fmt.Println(t.String())
+}
+
+// IMDb deployed awswaf and denies requests using the default Go user-agent (Go-http-client/1.1).
+// For now it still allows requests from a browser user-agent. Remain respectful, no spam, etc.
+const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+
+type customTransport struct {
+	http.RoundTripper
+}
+
+func (e *customTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	defer time.Sleep(time.Second)         // don't go too fast or risk being blocked by awswaf
+	r.Header.Set("Accept-Language", "en") // avoid IP-based language detection
+	r.Header.Set("User-Agent", userAgent)
+	return e.RoundTripper.RoundTrip(r)
 }
